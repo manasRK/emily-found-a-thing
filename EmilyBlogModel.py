22d0be87168d6340e7f29432f1355c2fb7ba8ebf
pete.bleackley <pete.bleackley@googlemail.com>
2013-10-16 17:18:26 +0000

    Added EmilyBlogModel.py and docstrings

---
+++ EmilyBlogModel.py
@@ -0,0 +1,90 @@
+import EmilyTreeNode
+import urllib2
+import feedparser
+import re
+from google.appengine.ext import db
+
+class EmilyBlogModel(db.Model):
+    """Model of the semantic structure of a blog"""
+    url=db.StringProperty()
+    
+    def __init__(self,url):
+        """Sets up a model of the blog at url"""
+        super(EmilyBlogModel,self).__init__()
+        self.words={}
+        self.sentences=[]
+        self.Tree=None
+        self.H=0
+        
+
+    def Similarity(self,other):
+        """Similarity metric for two blogs. An entropy-weighted variation
+           on a Tanimoto metric.
+           Sum the entropy-weighted similarity of the environments of all
+           words that occur in both blogs, and divide by the total entropy of
+           the two blogs"""
+        result=0
+        for word in self.Tree:
+            if word in other.Tree:
+                result+=self[word].Similarity(other[word])
+        return result/(self.H+other.H)
+
+    def GrowTree(self):
+        """Creates a tree structure representing the semantic relatinships
+           between the words in the blog"""
+        Similarities=[]
+        for word in self.words:
+            node={'node':self.words[word],
+                  'score':0}
+            for (i,node2) in enumerate(Similarities):
+                Sim=node['node'].Similarity(node2['node'])
+                if Sim>node['score']:
+                    node['score']=Sim
+                if Sim>node2['score']:
+                    Similarities[i]['score']=Sim
+            Similarities.append(node)
+        while len(Similarities)>1:
+            Similarities.sort(lambda x:-x['score'])
+            a=Similarities.pop()
+            b=Similarities.pop()
+            node={'node':a['node']+b['node'],
+                  'score':0}
+            for (i,node2) in enumerate(Similarities):
+                Sim=node['node'].Similarity(node2['node'])
+                if Sim>node['score']:
+                    node['score']=Sim
+                if Sim>node2['score']:
+                    Similarites[i]['score']=Sim
+            Similarities.append(node)
+        self.Tree=Similarities[0]
+        self.H=self.Tree.TotalEntropy()
+
+    def WordGraph(self):
+        """Returns the most significant words in the blog in a format suitable
+           to be plotted on a d3.js force-directed graph"""
+        result={'nodes':[{'word':word,
+                          'H':self.Tree[word].TotalEntropy()}
+                         for word in self.Tree
+                         if self.Tree[word].TotalEntropy()>1],
+                'links':[]}
+        for (i,node) in enumerate(result['nodes'][:-1]):
+            word1=node['word']
+            Node1=self.Tree[word1]
+            for j in range(i+1,len(result['nodes'])):
+                word2=result['nodes'][j]
+                Node2=self.Tree[word2]
+                result['links'].append({'source':i,
+                                        'target':j,
+                                        'strength':self.Tree.LinkEntropy(word1,word2))
+        return result
+
+    
+                           
+        
+                    
+                
+
+    
+
+    
+        
--- EmilyTreeNode.py
+++ EmilyTreeNode.py
@@ -1,7 +1,12 @@
 import math
 
 class EmilyTreeNode(object):
+    """Represents a word or set of words in a tree-based model of the
+       semantic structure of a document"""
     def __init__(self,words,sentences,N):
+        """words is the word or set of words represented by the node.
+           sentences is the set of sentences containing words.
+           N is the number of sentences in the document"""
         self.words=set(words)
         self.sentences=sentences
         self.H=None
@@ -12,6 +17,14 @@ class EmilyTreeNode(object):
         self.N=N
 
     def Entropy(self,other):
+        """ H=log2(P(AB)/(P(A)P(B))
+            where P(A) is the probability that a word from self.words occurs
+            in a given sentence
+            P(B) is the probability that a word from other.words occurs in a
+            given sentence
+            and P(AB) is the probability that words from self.words and
+            other words co-occur in the sentence.
+            This will be zero if the distributions of the words are independent"""
         result=self.loglen(self.sentences.intersection(other.sentences))
         result-=self.loglen(self.sentences)
         result-=self.loglen(other.sentences)
@@ -19,15 +32,21 @@ class EmilyTreeNode(object):
         return result
 
     def loglen(self,data):
+        """Log2 of len(data)+1 """
         return math.log(len(data)+1,2)
 
     def Tanimoto(self,other):
+        """Tanimoto metric between self.words and other.words
+           size of intersection/size of union"""
     return float(len(self.words & other.words))/len(self.word |other.words)
 
     def Similarity(self,other):
+        """Entropy weighted similarity of the environments in which words
+           represented by self and other occur"""
         return (self.Parent.H+other.Parent.H)*self.Parent.Tanimoto(other.Parent)
 
     def TotalEntropy(self):
+        """Total entropy of the words represented by self"""
         result=0
         if self.Left:
             result=self.Left.TotalEntropy()+self.Right.TotalEntropy()
@@ -35,7 +54,20 @@ class EmilyTreeNode(object):
             result=self.parent.H
         return result
 
+    def LinkEntropy(self,word1,word2):
+        """The entropy of the deepest node in the tree that contains both words.
+           Used for visualisation"""
+        result=self.H
+        if word1 in self.Left and word2 in self.Left:
+            result=self.Left.LinkEntropy(word1,word2)
+        elif word1 in self.Right and word2 in self.Right:
+            result=self.Right.LinkEntropy(word1,word2)
+        return result
+
     def __add__(self,other):
+        """Combines two tree nodes to give a third representing the union of the
+           sets of words represented by the two nodes.
+           Contains the two original nodes as children"""
         result=EmilyTreeNode(self.words | other.words,
                              self.sentences |other.sentences),
                              self.N)
@@ -45,11 +77,18 @@ class EmilyTreeNode(object):
         self.Parent=result
         other.Parent=result
         return result
+
+    def __iadd__(self,other):
+        """Add in place"""
+        self=self+other
+        return self
     
     def __contains__(self,word):
+        """Test for membership"""
         return word in self.words
 
     def __iter__(self):
+        """iterate over the terminal nodes of the tree"""
         if self.Left==None:
            yield self
         else:
@@ -57,5 +96,19 @@ class EmilyTreeNode(object):
                 yield Leaf
             for Leaf in self.Right:
                 yield Leaf
+
+    def __getitem__(self,word):
+        """returns the terminal node representing word"""
+        result=self
+        if len(self.words)>1:
+            if word in self.Left:
+                result=self.Left[word]
+            elif word in self.Right:
+                result=self.Right[word]
+            else:
+                raise KeyError(word)
+        return result
+
+                
     
      
